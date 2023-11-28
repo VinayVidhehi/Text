93a8bcde9f08687a815bb11e40842dcbc2b449b2
VinayVidhehi <vinayvidhehi@gmail.com>
2023-11-28 10:57:05 +0530

    final update on frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,66 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const http = require("http");
+const socketIO = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors"); // Import the cors module
+
+try {
+  mongoose.connect("mongodb://localhost:27017/db");
+  console.log("connected to mongodb");
+} catch (err) {
+  console.log(err.message);
+}
 
 const app = express();
-app.use(cors());
+app.use(bodyParser.json());
+app.use(cors()); // Enable CORS for all routes
 const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
   // Listen for chat messages
-  socket.on('chat message', (msg) => {
-    console.log('Message from client:', msg);
+  socket.on("chat-message", (msg) => {
+    console.log("Message from client:", msg);
 
+    //console.log(JSON.stringify(msg.receiver));
     // Broadcast the message to all connected clients
-    io.emit('chat message', msg);
+    io.emit(JSON.stringify(msg.receiver), msg);
   });
 
   // Handle disconnection
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
+});
+
+const userschema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userschema);
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send({ users });
+  } catch (error) {
+    console.log("error fetching users, try again later ", error);
+    res.status(500).send({message:"internal server error"});
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = new User({
+    username,
+    password,
+  });
+  await newUser.save();
+  res.status(200).send({ message: "user saved" });
 });
 
 // Start the server
